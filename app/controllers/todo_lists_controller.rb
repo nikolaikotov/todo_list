e434f732cc466f119a7dccef1bfa9668910b4ee9
nikolaikotov <nikolaikotov4@gmail.com>
2020-04-23 21:13:22 +0300

    frst cmt

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -4,7 +4,9 @@ class TodoListsController < ApplicationController
   # GET /todo_lists
   # GET /todo_lists.json
   def index
-    @todo_lists = TodoList.all
+    if user_signed_in?
+      @todo_lists = TodoList.where(:user_id => current_user.id)
+    end  
   end
 
   # GET /todo_lists/1
@@ -14,7 +16,7 @@ class TodoListsController < ApplicationController
 
   # GET /todo_lists/new
   def new
-    @todo_list = TodoList.new
+    @todo_list = current_user.todo_lists.build
   end
 
   # GET /todo_lists/1/edit
@@ -24,7 +26,7 @@ class TodoListsController < ApplicationController
   # POST /todo_lists
   # POST /todo_lists.json
   def create
-    @todo_list = TodoList.new(todo_list_params)
+    @todo_list = current_user.todo_lists.build(todo_list_params)
 
     respond_to do |format|
       if @todo_list.save
